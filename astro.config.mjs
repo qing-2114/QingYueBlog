70d30a4ac0fbb31dc2fae6f3159eea52493106a5
qing-2114 <158837519+qing-2114@users.noreply.github.com>
2026-09-02 15:04:34 +0800

    Configure project Pages path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://qing-2114.github.io',
+	site: 'https://qing-2114.github.io/QingYueBlog',
+	base: '/QingYueBlog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
